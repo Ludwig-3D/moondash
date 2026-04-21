1e277fabb323bb9ca34f57a0b981bcaa8f4f07f7
tludwig <business@tludwig.dev>
2026-04-21 16:26:45 +0200

    feat: prepare settings page

--- src/pages/settings.vue
+++ src/pages/settings.vue
@@ -1,11 +1,48 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import { useI18n } from 'vue-i18n'
+
+const tab = ref('network')
+const { t } = useI18n()
+
 
 </script>
 
 <template>
-CC
+  <v-main>
+    <v-app-bar elevation="0" color="transparent" density="compact">
+      <v-tabs v-model="tab" color="primary">
+        <v-tab value="network">{{ t('settings.tab.network') }}</v-tab>
+        <v-tab value="settings">{{ t('settings.tab.settings') }}</v-tab>
+        <v-tab value="tools">{{ t('settings.tab.tools') }}</v-tab>
+      </v-tabs>
+    </v-app-bar>
+
+    <v-tabs-window v-model="tab" class="panel-container">
+      <v-tabs-window-item value="network">
+        <v-sheet class="pa-0 pr-3 pb-3 pt-" color="red">
+
+        </v-sheet>
+      </v-tabs-window-item>
+      <v-tabs-window-item value="settings">
+        <v-sheet class="pa-0 pr-3 pb-3 pt-" color="blue">
+
+        </v-sheet>
+      </v-tabs-window-item>
+      <v-tabs-window-item value="tools">
+        <v-sheet class="pa-0 pr-3 pb-3 pt-" color="green">
+
+        </v-sheet>
+      </v-tabs-window-item>
+
+    </v-tabs-window>
+  </v-main>
 </template>
 
 <style scoped>
-
+.panel-container {
+  height: 100%;
+  min-height: 0;
+  max-height: 100%;
+}
 </style>
